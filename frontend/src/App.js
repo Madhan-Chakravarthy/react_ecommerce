f1addcbb637e3b6bc48251a8f321c98d8c7a49aa
MadhanChakravarthy <madhanchakravarthy1999@gmail.com>
2021-11-23 23:16:47 +0530

    init

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Footer from './components/Footer';
+import Header from './components/Header';
+
+
+const App = ()=> {
+  return (
+    <>
+    <Header/>
+    <main>
+    <h1>Welcome</h1>
+    
+    </main>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
