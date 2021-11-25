33787a41122de23e6a13b43b82b7efcadcc7d671
MadhanChakravarthy <madhanchakravarthy1999@gmail.com>
2021-11-25 15:48:39 +0530

    HomeScreen with product list

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import { Container, Row, Col } from 'react-bootstrap'
 const Footer = () => {
     return (
         <footer>
-            Footer
+            <Container>
+                <Row>
+                    <Col className='text-center py-3'>
+                        Copy Right &copy; React eCommerce
+                    </Col>
+                </Row>
+            </Container>
+
         </footer>
     )
 }
