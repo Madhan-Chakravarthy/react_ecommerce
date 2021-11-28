a15f68db76cf3b1da9f575f9d1fdc7d786bf616a
MadhanChakravarthy <madhanchakravarthy1999@gmail.com>
2021-11-28 17:06:22 +0530

    LinkContainer

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -3,38 +3,38 @@ import PropTypes from 'prop-types'
 
 
 
-const Rating = ({ rating, text,color }) => {
+const Rating = ({ rating, text, color }) => {
     return (
         <div className='rating'>
-            <span><i style={{color}}
-                    className={
-                rating >= 1 ? 'fas fa-star'
-                    : rating >= 0.5 ? 'fas fa-star-half-alt'
-                        : 'far fa-star'}></i>
+            <span><i style={{ color }}
+                className={
+                    rating >= 1 ? 'fas fa-star'
+                        : rating >= 0.5 ? 'fas fa-star-half-alt'
+                            : 'far fa-star'}></i>
             </span>
-            <span><i style={{color}}
-             className={
-                rating >= 2 ? 'fas fa-star'
-                    : rating >= 1.5 ? 'fas fa-star-half-alt'
-                        : 'far fa-star'}></i>
+            <span><i style={{ color }}
+                className={
+                    rating >= 2 ? 'fas fa-star'
+                        : rating >= 1.5 ? 'fas fa-star-half-alt'
+                            : 'far fa-star'}></i>
             </span>
-            <span><i style={{color}}
-             className={
-                rating >= 3 ? 'fas fa-star'
-                    : rating >= 2.5 ? 'fas fa-star-half-alt'
-                        : 'far fa-star'}></i>
+            <span><i style={{ color }}
+                className={
+                    rating >= 3 ? 'fas fa-star'
+                        : rating >= 2.5 ? 'fas fa-star-half-alt'
+                            : 'far fa-star'}></i>
             </span>
-            <span><i style={{color}}
-             className={
-                rating >= 4 ? 'fas fa-star'
-                    : rating >= 3.5 ? 'fas fa-star-half-alt'
-                        : 'far fa-star'}></i>
+            <span><i style={{ color }}
+                className={
+                    rating >= 4 ? 'fas fa-star'
+                        : rating >= 3.5 ? 'fas fa-star-half-alt'
+                            : 'far fa-star'}></i>
             </span>
-            <span><i style={{color}}
-             className={
-                rating >= 5 ? 'fas fa-star'
-                    : rating >= 4.5 ? 'fas fa-star-half-alt'
-                        : 'far fa-star'}></i>
+            <span><i style={{ color }}
+                className={
+                    rating >= 5 ? 'fas fa-star'
+                        : rating >= 4.5 ? 'fas fa-star-half-alt'
+                            : 'far fa-star'}></i>
             </span>
             <span>{text && text}
             </span>
@@ -47,8 +47,8 @@ Rating.defaultProps = {
     color: '#f8e825'
 }
 
-Rating.prototype ={
-    rating:PropTypes.number.isRequired,
+Rating.prototype = {
+    rating: PropTypes.number.isRequired,
     text: PropTypes.string.isRequired,
     color: PropTypes.string
 }
---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const ProductScreen = props => {
+    return (
+        <div>
+            <h1>product page </h1>
+        </div>
+    )
+}
+
+
+
+export default ProductScreen
